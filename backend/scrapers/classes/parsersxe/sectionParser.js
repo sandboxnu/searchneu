2fa95b0bd6920a4dfabdd7cafe03c71c323c3787
meebs <alex@takayamasan.com>
2020-11-15 15:04:15 -0500

    add back online to sectionParser

--- backend/scrapers/classes/parsersxe/sectionParser.js
+++ backend/scrapers/classes/parsersxe/sectionParser.js
@@ -46,6 +46,7 @@ class SectionParser {
       waitCapacity: SR.waitCapacity,
       waitRemaining: SR.waitAvailable,
       classType: SR.scheduleTypeDescription,
+      online: SR.campusDescription === 'Online',
       campus: SR.campusDescription,
       honors: SR.sectionAttributes.some((a) => { return a.description === 'Honors'; }),
       url: 'https://wl11gp.neu.edu/udcprod8/bwckschd.p_disp_detail_sched'

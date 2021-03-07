c665f7c978c3a8104da5137b7abff40d04822f00
Andrew Dai <dai.an@husky.neu.edu>
2021-03-06 20:24:08 -0500

    add 404 icons, text and button

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,12 @@
 import { ReactElement } from 'react';
+import Four04 from '../components/icons/404.svg';
 
 export default function NotFoundPage(): ReactElement {
-  return <h1>404: YOU GOT LIGMA</h1>;
+  return (
+    <div className="four04-container">
+      <div className="four04-title-text">something's borked</div>
+      <Four04 />
+      <div className="four04-return-button">BACK TO HOME</div>
+    </div>
+  );
 }

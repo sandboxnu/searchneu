2a048ce69ce66100dcd631831ecdd833337afcaf
Andrew Dai <dai.an@husky.neu.edu>
2021-03-06 20:30:05 -0500

    add directing to home page with 404 button and fix styling

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import Link from 'next/link';
 import Four04 from '../components/icons/404.svg';
 
 export default function NotFoundPage(): ReactElement {
@@ -6,7 +7,9 @@ export default function NotFoundPage(): ReactElement {
     <div className="four04-container">
       <div className="four04-title-text">something's borked</div>
       <Four04 />
-      <div className="four04-return-button">BACK TO HOME</div>
+      <Link href="/">
+        <div className="four04-return-button">BACK TO HOME</div>
+      </Link>
     </div>
   );
 }

b2bef6843e59296b1b5205994bef0297f02476ea
Andrew Dai <dai.an@husky.neu.edu>
2021-08-18 19:45:57 -0400

    add ability to use yaml files in project
    adds configuration for yaml files in tests, webpacker, and
    necessary packages for loading yaml files. Yaml files are
    generally more readable, making them a good choice for
    config files.

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,8 @@ declare module '*.svg' {
   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+declare module '*.yml' {
+  const data: any;
+  export default data;
+}

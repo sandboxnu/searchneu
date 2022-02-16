b309b0a10f086c27b47b7781c0d4631e7f609b35
Sebastian Wittrock <wittrock.s@northeastern.edu>
2022-02-16 01:18:25 -0500

    Reverted test files

--- components/tests/pages/Result.test.tsx
+++ components/tests/pages/Result.test.tsx
@@ -10,56 +10,21 @@ import React from 'react';
 import { QueryParamProvider } from 'use-query-params';
 import Results from '../../../pages/[campus]/[termId]/search/[query]';
 
-describe('cs test query', () => {
-  beforeAll(() => {
-    jest.mock('next/router', () => ({
-      useRouter: () => ({
-        query: { campus: 'NEU', termId: '202160', query: 'cs' },
-      }),
-      useQueryParam: () => false,
-    }));
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    query: { campus: 'NEU', termId: '202160', query: 'cs' },
+  }),
+  useQueryParam: () => false,
+}));
 
-    Enzyme.configure({ adapter: new Adapter() });
-  });
+Enzyme.configure({ adapter: new Adapter() });
 
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
+it('should render a section', () => {
+  const result = mount(
+    <QueryParamProvider>
+      <Results />
+    </QueryParamProvider>
+  );
 
-  it('should render a section', () => {
-    const result = mount(
-      <QueryParamProvider>
-        <Results />
-      </QueryParamProvider>
-    );
-
-    expect(result.debug()).toMatchSnapshot();
-  });
-});
-
-describe('testing honors query', () => {
-  beforeAll(() => {
-    jest.mock('next/router', () => ({
-      useRouter: () => ({
-        query: { campus: 'NEU', termId: '202230', query: 'PHIL 1145' },
-      }),
-      useQueryParam: () => false,
-    }));
-
-    Enzyme.configure({ adapter: new Adapter() });
-  });
-
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
-  it('should render a section with honors', () => {
-    const result = mount(
-      <QueryParamProvider>
-        <Results />
-      </QueryParamProvider>
-    );
-
-    expect(result.debug()).toMatchSnapshot();
-  });
+  expect(result.debug()).toMatchSnapshot();
 });

0325c56dbbb4dabe51918c1f6e2f487842e21493
Sebastian Wittrock <wittrock.s@northeastern.edu>
2022-10-13 00:05:59 -0400

    removed linting issues

--- components/HomePage/HomeSearch.tsx
+++ components/HomePage/HomeSearch.tsx
@@ -10,7 +10,6 @@ import SearchBar from '../ResultsPage/SearchBar';
 import SearchDropdown from '../ResultsPage/SearchDropdown';
 import { Campus } from '../types';
 import getTermInfos from '../../utils/TermInfoProvider';
-import SearchInfoIcon from '../common/SearchInfoIcon';
 
 interface HomeSearchProps {
   termId: string;

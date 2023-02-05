d277be2f7ceeb6a1a82951ec40f92f40461507d7
Pranav Phadke <pranavmphadke@gmail.com>
2023-02-04 21:03:03 -0500

    more styling

--- pages/apiError.tsx
+++ pages/apiError.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement, useState } from 'react';
 import Boston from '../components/icons/boston.svg';
 import FooterFeedbackModal from '../components/FeedbackModal';
-import CryingHusky from '../components/icons/crying-husky.svg';
-import HuskyDollar from '../components/icons/husky-dollar.svg';
+import Husky from '../components/icons/Husky';
+import { Campus } from '../components/types';
+import Image from 'next/image';
+import { Button } from 'semantic-ui-react';
 
 /**
  * Page to indicate the api is down.
@@ -17,34 +19,67 @@ export default function ApiErrorPage(): ReactElement {
   };
 
   return (
-    <div>
-      <div className={containerClassnames}>
-        <div // TODO: Take this out and restyle this monstrosity from scratch
-          className="ui center spacing aligned icon header topHeader"
-        >
-          <div className="down-text-container">
-            <div className="down-title-text">Oh man!</div>
-            <div className="down-sub-title-text">Something went wrong...</div>
-            <div className="down-text">{"Don't worry, we're on it"}</div>
-            <div>
-              <h3> An Error Occurred : </h3>
-              <a role="button" onClick={toggleModal}>
-                Report a bug
-              </a>
-            </div>
-          </div>
+    <>
+      <div>
+        <div className={containerClassnames}>
+          <a
+            href="https://www.sandboxnu.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="sandboxLogoContainer"
+          >
+            <Image
+              src="/images/sandbox-logo.png"
+              alt="sandbox logo"
+              width={47}
+              height={61}
+            />
+          </a>
 
-          <HuskyDollar className="huskyDollar" aria-label="logo" />
-          <CryingHusky className="cryingHusky" aria-label="logo" />
-          <div className="bostonContainer">
-            <Boston className="boston" aria-label="logo" />
+          <div>
+            <div // TODO: Take this out and restyle this monstrosity from scratch
+              className="ui center spacing aligned icon header topHeader"
+            >
+              <div className="centerTextContainer">
+                <div className="down-title-text">Oh man!</div>
+                <div className="down-sub-title-text">
+                  Something went wrong...
+                </div>
+                <div>
+                  <div className="down-sub-title-text">
+                    {"Don't worry, we're on it"}
+                    <br />
+                    <br />
+                    <Button
+                      color="red"
+                      className="button"
+                      onClick={toggleModal}
+                    >
+                      SEND SUPPORT REQUEST
+                    </Button>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/sandboxnu/searchneu"
+                    >
+                      &nbsp;Create issue on GitHub
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <Husky className="husky" campus={Campus.NEU} aria-label="husky" />
+              <div className="bostonContainer">
+                <Boston className="boston" aria-label="logo" />
+              </div>
+            </div>
+            <FooterFeedbackModal
+              toggleForm={toggleModal}
+              feedbackModalOpen={modalOpen}
+            />
           </div>
         </div>
       </div>
-      <FooterFeedbackModal
-        toggleForm={toggleModal}
-        feedbackModalOpen={modalOpen}
-      />
-    </div>
+    </>
   );
 }

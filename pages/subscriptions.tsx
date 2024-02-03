a3d4a8887eeb81ba750df715bd6000ad82dc740f
Pranav Phadke <pranavmphadke@gmail.com>
2024-02-02 20:14:48 -0500

    Rewrote logic for finding sections
    We now use the section ids and course ids in userInfo to find all courses that a user has subscribed to (whether directly to the course for new section alerts or to associated sections for seat opening alerts)

--- pages/subscriptions.tsx
+++ pages/subscriptions.tsx
@@ -3,13 +3,12 @@ import { useRouter } from 'next/router';
 import { gqlClient } from '../utils/courseAPIClient';
 import { PacmanLoader } from 'react-spinners';
 import useUserInfo from '../utils/useUserInfo';
-import { Section, SubscriptionCourse } from '../components/types';
+import { SubscriptionCourse } from '../components/types';
 import { ClassCard } from '../components/SubscriptionsPage/ClassCard';
 
 export default function SubscriptionsPage(): ReactElement {
-  const [userInfo, isLoading] = useUserInfo();
-  const [sections, setSections] = useState(new Map());
-  const [classes, setClasses] = useState(new Map());
+  const [userInfo, isLoading, fetchUserInfo] = useUserInfo();
+  const [classes, setClasses] = useState(new Map<string, SubscriptionCourse>());
 
   // is the course / section data still fetching
   const [isFetching, setIsFetching] = useState(true);
@@ -25,9 +24,9 @@ export default function SubscriptionsPage(): ReactElement {
       return;
     }
 
-    const classSectionMapping = new Map<string, Section[]>();
     const classMapping = new Map<string, SubscriptionCourse>();
 
+    // This uses subscribed course ids to find the associated courses and their sections
     const fetchCourseNotifs = async (): Promise<void> => {
       for (const courseId of userInfo.courseIds) {
         const result = await gqlClient.getCourseInfoByHash({
@@ -38,16 +37,38 @@ export default function SubscriptionsPage(): ReactElement {
         const courseCode =
           result.classByHash.subject + result.classByHash.classId;
 
-        classSectionMapping.set(courseCode, []);
+        const subject = result.classByHash.subject;
+        const classId = result.classByHash.classId;
+        const host = result.classByHash.host;
+        const termId = result.classByHash.termId;
 
+        const filteredSections = result.classByHash.sections
+          .filter((s) => {
+            return s.seatsRemaining <= 5;
+          })
+          .map((s) => {
+            return {
+              ...s,
+              campusDescription: '',
+              online: false,
+              subject: subject,
+              classId: classId,
+              host: host,
+              termId: termId,
+            };
+          });
         classMapping.set(courseCode, {
-          subject: result.classByHash.subject,
-          classId: result.classByHash.classId,
+          subject: subject,
+          classId: classId,
+          host: host,
           name: result.classByHash.name,
+          lastUpdateTime: result.classByHash.lastUpdateTime,
+          sections: filteredSections,
         });
       }
     };
 
+    // This uses subscribed section ids to find the associated section and the associated course for that section
     const fetchSectionNotifs = async (): Promise<void> => {
       for (const sectionId of userInfo.sectionIds) {
         const result = await gqlClient.getSectionInfoByHash({
@@ -55,50 +76,53 @@ export default function SubscriptionsPage(): ReactElement {
         });
         const courseCode =
           result.sectionByHash.subject + result.sectionByHash.classId;
-        const sectionObj: Section = {
-          crn: result.sectionByHash.crn,
-          profs: result.sectionByHash.profs,
-          meetings: result.sectionByHash.meetings,
-          seatsRemaining: result.sectionByHash.seatsRemaining,
-          seatsCapacity: result.sectionByHash.seatsCapacity,
-          waitRemaining: result.sectionByHash.seatsRemaining,
-          waitCapacity: result.sectionByHash.seatsCapacity,
-          honors: result.sectionByHash.honors,
-          campus: result.sectionByHash.campus,
-          campusDescription: '',
-          lastUpdateTime: result.sectionByHash.lastUpdateTime,
-          url: result.sectionByHash.url,
-          online: null,
-        };
 
-        if (classSectionMapping.has(courseCode)) {
-          classSectionMapping.get(courseCode).push(sectionObj);
-        } else {
+        // If course has already been found in fetchCourseNotifs(), continue
+        if (!classMapping.has(courseCode)) {
           const sectionHashSlice = sectionId.split('/');
           const courseHash = sectionHashSlice.slice(0, -1).join('/');
 
-          // sections don't have information on class name so we have to search for course results
           const courseResult = await gqlClient.getCourseInfoByHash({
             hash: courseHash,
           });
 
-          classMapping.set(courseCode, {
-            subject: courseResult.classByHash.subject,
-            classId: courseResult.classByHash.classId,
-            name: courseResult.classByHash.name,
-          });
+          const subject = courseResult.classByHash.subject;
+          const classId = courseResult.classByHash.classId;
+          const host = courseResult.classByHash.host;
+          const termId = courseResult.classByHash.termId;
 
-          classSectionMapping.set(courseCode, [sectionObj]);
+          const filteredSections = courseResult.classByHash.sections
+            .filter((s) => {
+              return s.seatsRemaining <= 5;
+            })
+            .map((s) => {
+              return {
+                ...s,
+                campusDescription: '',
+                online: false,
+                subject: subject,
+                classId: classId,
+                host: host,
+                termId: termId,
+              };
+            });
+          classMapping.set(courseCode, {
+            subject: subject,
+            classId: classId,
+            host: host,
+            name: courseResult.classByHash.name,
+            lastUpdateTime: courseResult.classByHash.lastUpdateTime,
+            sections: filteredSections,
+          });
         }
       }
     };
 
     const fetchSubscriptions = async (): Promise<void> => {
       try {
-        await fetchSectionNotifs();
         await fetchCourseNotifs();
+        await fetchSectionNotifs();
         setClasses(classMapping);
-        setSections(classSectionMapping);
         setIsFetching(false);
       } catch (e) {
         console.log(e);
@@ -117,14 +141,15 @@ export default function SubscriptionsPage(): ReactElement {
           <div className="Results_Container">
             <div className="Results_MainWrapper">
               <div className="Results_Main">
-                <h2>This page is a work in progress!</h2>
-                {Array.from(sections).map(([courseCode, sections]) => {
-                  const course = classes.get(courseCode);
+                <h2>Subscriptions</h2>
+                {Array.from(classes).map(([courseCode, course]) => {
                   return (
                     <ClassCard
                       key={courseCode}
                       course={course}
-                      sections={sections}
+                      sections={course.sections}
+                      userInfo={userInfo}
+                      fetchUserInfo={fetchUserInfo}
                     />
                   );
                 })}

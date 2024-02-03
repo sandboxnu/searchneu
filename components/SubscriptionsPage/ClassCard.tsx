c25ae45bf5b9f1cadc891965722e723bf5f45f64
Pranav Phadke <pranavmphadke@gmail.com>
2024-02-02 20:16:34 -0500

    Updated class card and useUserInfo hook

--- components/SubscriptionsPage/ClassCard.tsx
+++ components/SubscriptionsPage/ClassCard.tsx
@@ -1,35 +1,105 @@
 import { ReactElement } from 'react';
-import { Section } from '../types';
+import { Section, UserInfo } from '../types';
+import { LastUpdated } from '../common/LastUpdated';
+import { DesktopSectionPanel } from '../ResultsPage/Results/SectionPanel';
+import { getFormattedSections } from '../ResultsPage/ResultsLoader';
+import { cloneDeep } from 'lodash';
+
+type ClassCardWrapperType = {
+  headerLeft: ReactElement;
+  headerRight?: ReactElement;
+  body?: ReactElement;
+  afterBody?: ReactElement;
+};
+
+const ClassCardWrapper = ({
+  headerLeft,
+  headerRight,
+  body,
+  afterBody,
+}: ClassCardWrapperType): ReactElement => {
+  return (
+    <div className="SearchResult">
+      <div className="SearchResult__header">
+        <div className="SearchResult__header--left">{headerLeft}</div>
+        {headerRight}
+      </div>
+      <div className="SearchResult__panel">{body}</div>
+      {afterBody}
+    </div>
+  );
+};
 
 type ClassCardType = {
-  course: { subject: string; classId: string; name: string };
+  course: {
+    subject: string;
+    classId: string;
+    name: string;
+    host: string;
+    lastUpdateTime: number;
+  };
   sections: Section[];
+  userInfo: UserInfo;
+  fetchUserInfo: () => void;
 };
 
 export const ClassCard = ({
   course,
   sections,
+  userInfo,
+  fetchUserInfo,
 }: ClassCardType): ReactElement => {
-  return (
-    <>
-      <div>
-        <div>{course.subject + ' ' + course.classId + ': ' + course.name}</div>
-      </div>
+  const sectionsFormated: Section[] = getFormattedSections(cloneDeep(sections));
 
-      {sections.map((section) => {
-        return (
-          <div
-            key={section.crn}
-            style={{
-              display: 'flex',
-              justifyContent: 'space-around',
-            }}
-          >
-            <div>CRN: {section.crn}</div>
-            <div>Professor: {section.profs}</div>
-          </div>
-        );
-      })}
-    </>
+  return (
+    <ClassCardWrapper
+      headerLeft={
+        <>
+          <span className="SearchResult__header--classTitle">
+            {course.subject} {course.classId}: {course.name}
+          </span>
+          <LastUpdated
+            host={course.host}
+            prettyUrl={course.prettyUrl} //Ignore this for now, the current link we use is outdated anyway.
+            lastUpdateTime={course.lastUpdateTime}
+            className="SearchResult__header--sub"
+          />
+        </>
+      }
+      headerRight={<button>Unsubscribe</button>}
+      afterBody={
+        <>
+          <table className="SearchResult__sectionTable">
+            <thead>
+              <tr>
+                <th>
+                  <div
+                    className="inlineBlock"
+                    data-tip="Course Reference Number"
+                  >
+                    CRN
+                  </div>
+                </th>
+                <th> Professors </th>
+                <th> Meetings </th>
+                <th> Campus </th>
+                <th> Seats </th>
+                {userInfo && <th> Notifications </th>}
+              </tr>
+            </thead>
+            <tbody>
+              {sectionsFormated.map((section) => (
+                <DesktopSectionPanel
+                  key={section.crn}
+                  section={section}
+                  userInfo={userInfo}
+                  fetchUserInfo={fetchUserInfo}
+                />
+              ))}
+            </tbody>
+          </table>
+        </>
+      }
+    />
   );
 };
--- utils/useUserInfo.ts
+++ utils/useUserInfo.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { UserInfo } from '../components/types';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
@@ -8,32 +8,36 @@ import axios from 'axios';
  * @returns a list of [UserInfo: UserInfo, isLoading (boolean: representing if the data is still being fetched)]
  */
 
-const useUserInfo = (): [UserInfo | null, boolean] => {
+const useUserInfo = (): [UserInfo | null, boolean, () => void] => {
   const [cookies, setCookies] = useState(new Cookies());
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const fetchUserInfo = async (): Promise<void> => {
+    const token = cookies.get('SearchNEU JWT');
+    if (token) {
+      await axios
+        .get(
+          `${process.env.NEXT_PUBLIC_NOTIFS_ENDPOINT}/user/subscriptions/${token}`
+        )
+        .then(({ data }) => {
+          setUserInfo({ token, ...data });
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const token = cookies.get('SearchNEU JWT');
-      if (token) {
-        await axios
-          .get(
-            `${process.env.NEXT_PUBLIC_NOTIFS_ENDPOINT}/user/subscriptions/${token}`
-          )
-          .then(({ data }) => {
-            setUserInfo({ token, ...data });
-          })
-          .catch((e) => {
-            console.log(e);
-          });
-      }
+    const fetchData = async (): Promise<void> => {
+      await fetchUserInfo();
       setIsLoading(false);
     };
     fetchData();
   }, [cookies]);
 
-  return [userInfo, isLoading];
+  return [userInfo, isLoading, fetchUserInfo];
 };
 
 export default useUserInfo;

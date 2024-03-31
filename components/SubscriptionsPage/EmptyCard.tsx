67d82ee3c98bcf1569029317906065f730750419
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2024-03-31 16:57:57 -0400

    Add styling and assets

--- components/SubscriptionsPage/EmptyCard.tsx
+++ components/SubscriptionsPage/EmptyCard.tsx
@@ -13,11 +13,13 @@ export const EmptyCard = (): ReactElement => {
             <div className="Results_Main__EmptyCard">
               <ClassCardWrapper
                 headerLeft={
-                  <div className="">
-                    <b className="Results_Main__EmptyCard_Title">
-                      You currently have no notifications. Hoosky sad :(
-                    </b>
+                  <div className="Results_Main__EmptyCard_Text">
                     <p>
+                      <div className="Results_Main__EmptyCard_Text_Title">
+                        <b>
+                          You currently have no notifications. Hoosky sad :(
+                        </b>
+                      </div>
                       Be the first to know when new classes and sections drop!
                     </p>
                   </div>

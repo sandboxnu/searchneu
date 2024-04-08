d22ecf26e78e22a8865e4e28ff38f186d6ef6a30
Pranav Phadke <pranavmphadke@gmail.com>
2024-04-08 15:12:51 -0400

    don't need useState here

--- utils/useUserInfo.ts
+++ utils/useUserInfo.ts
@@ -13,7 +13,7 @@ type useUserInfoReturn = {
 
 // Custom hook to maintain all userInfo related utility functions
 const useUserInfo = (): useUserInfoReturn => {
-  const [cookies, setCookies] = useState(new Cookies());
+  const cookies = new Cookies();
   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
   const [isUserInfoLoading, setIsUserInfoLoading] = useState(true);
 

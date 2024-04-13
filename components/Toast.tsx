4fd14bf0c6ea2ce81b262a512f3f89668c7a7813
Pranav Phadke <pranavmphadke@gmail.com>
2024-04-13 18:01:55 -0400

    ToastWrapper component

---
+++ components/Toast.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, useEffect, useState } from 'react';
+import X from './icons/X.svg';
+
+interface ToastProps {
+  message: ReactElement;
+  duration?: number;
+  infiniteLength?: boolean;
+  image?: ReactElement;
+}
+
+export default function Toast({
+  message,
+  duration = 20,
+  infiniteLength = false,
+  image = null,
+}: ToastProps): ReactElement {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (!infiniteLength) {
+      const timeout = setTimeout(() => {
+        setIsVisible(false);
+      }, duration);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [duration, infiniteLength]);
+
+  return isVisible ? (
+    <div className="toast-body">
+      {image}
+      {message}
+      <button
+        className="phone-modal__action-btn phone-modal__action-btn--x"
+        onClick={() => setIsVisible(false)}
+      >
+        <X />
+      </button>
+    </div>
+  ) : null;
+}

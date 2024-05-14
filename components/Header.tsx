af8bde95b41224fb231ce7134c023de3a29bf0d4
Pranav Phadke <pranavmphadke@gmail.com>
2024-05-14 00:32:01 -0400

    Memoize DropDownMenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import React, {
   ReactElement,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
@@ -69,14 +70,16 @@ export const DropdownMenuWrapper = ({
   onSignIn,
 }: DropdownMenuWrapperProps): ReactElement => {
   const [showModal, setShowModal] = useState(false);
-  const [showMenuDropdown, setShowMenuDropdown] = useState(false);
+  const showMenuDropdown = useRef(false);
   const dropdownRef = useRef(null);
   const router = useRouter();
 
+  const [userLoggedOut, setUserLoggedOut] = useState(false);
+  
   useEffect(() => {
     const handleCloseDropdown = (event: Event): void => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setShowMenuDropdown(false);
+        showMenuDropdown.current = false;
       }
     };
 
@@ -91,23 +94,23 @@ export const DropdownMenuWrapper = ({
     setShowModal(true);
   };
 
-  const toggleMenuDropdown = (): void => {
-    setShowMenuDropdown(!showMenuDropdown);
-  };
+  const DropDownMenu = useMemo(() => {
+    const toggleMenuDropdown = (): void => {
+      showMenuDropdown.current = !showMenuDropdown.current;
+    };
 
-  const DropDownMenu = (): ReactElement => {
-    return (
+    const MemoizedDropDownMenu = (): ReactElement => (
       <div className="user-menu">
         <div className={'user-menu__icon-wrapper'}>
           <>
-            {userInfo && !macros.isMobile && (
+            {/* {userInfo && !macros.isMobile && (
               <button
                 onClick={() => router.push('/subscriptions')}
                 className="user-menu__button"
               >
                 Notifications
               </button>
-            )}
+            )} */}
             {/* Still need to create FAQ page */}
             {/* <button>FAQ</button> */}
           </>
@@ -120,9 +123,9 @@ export const DropdownMenuWrapper = ({
           </div>
         </div>
 
-        {showMenuDropdown && (
-          <div ref={dropdownRef} className="user-menu__dropdown">
-            <span className="user-menu__item" onClick={onSignOut}>
+        {showMenuDropdown.current && (
+          <div ref={dropdownRef} onClick={() => {onSignOut(), setUserLoggedOut(true)}} className="user-menu__dropdown">
+            <span className="user-menu__item">
               <Exit style={{ marginRight: '8px' }} />
               <div className="user-menu__item--text-container">
                 <span className="user-menu__item--text">Sign out</span>
@@ -135,11 +138,16 @@ export const DropdownMenuWrapper = ({
         )}
       </div>
     );
-  };
+
+    MemoizedDropDownMenu.displayName = 'DropDownMenu';
+
+    return MemoizedDropDownMenu;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[showMenuDropdown.current, userInfo?.token]);
 
   return (
     <>
-      {userInfo ? (
+      {userInfo && !userLoggedOut ? (
         <DropDownMenu />
       ) : (
         <>
@@ -147,10 +155,10 @@ export const DropdownMenuWrapper = ({
           <SignUpModal
             visible={showModal}
             onCancel={() => setShowModal(false)}
-            onSignIn={onSignIn}
+            onSignIn={(token: string) => {onSignIn(token); setUserLoggedOut(false)}}
             onSuccess={() => {
               setShowModal(false);
-              setShowMenuDropdown(false);
+              showMenuDropdown.current = false;
             }}
           />
         </>

0dad192ae1ce0a91a7e9d79e4bb141cc403748e7
Ananya Patil <106994008+ananyaspatil@users.noreply.github.com>
2024-12-04 17:48:06 -0500

    Ap/no notifications husky page (#265)
    * Add empty subscriptions logic
    
    * Create EmptyCard component
    
    * Add styling and assets
    
    * Update styling of EmptyCard
    
    * Modified styling, added on hover image change functionality
    
    * edited line-heights
    
    * resolved sass-lint errors
    
    * actually resolved sass-lint errors
    
    * really actually resolved sass-lint errors this time
    
    * Update Empyt Card to include term data.
    
    * Move styles from _Results to new _EmptyCard to declutter.
    
    * Remove empty card styles from _Results
    
    * Revert styles, fix empty card props. subscriptions page requires retrieval of Term name.
    
    * Empty Card Semester Name updates automatically
    
    Properly migrate styles to new _EmptyCard file
    
    * - remove unused leading-trim module
    
    - fix styles in classcard, use function notation
    
    - Redirect Empty card to empty results page.
    
    * remove default from ClassCard export
    
    * Add unsubscribe button functionality
    
    * Notif course fetching refactor
    
    * Remove local config
    
    * Remove yarn install state
    
    * Add pill to subscription class card
    
    * fix lint colors issue
    
    ---------
    
    Co-authored-by: Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
    Co-authored-by: cherman27 <carterherman27@gmail.com>
    Co-authored-by: mehallhm <michaelhmehall@gmail.com>

---
+++ components/SubscriptionsPage/EmptyCard.tsx
@@ -0,0 +1,74 @@
+import { ReactElement } from 'react';
+import React, { useState } from 'react';
+import { ClassCardWrapper } from './ClassCard';
+import { useRouter } from 'next/router';
+import Circular from '../icons/circular.svg';
+import CryingHusky from '../icons/crying-husky.svg';
+import HappyHusky from '../icons/happy-husky.svg';
+import getTermInfosWithError from '../../utils/TermInfoProvider';
+import { getTermName } from '../terms';
+
+export const EmptyCard = (): ReactElement => {
+  const router = useRouter();
+  const [isHovering, setIsHovering] = useState(false);
+
+  const termInfos = getTermInfosWithError().termInfos;
+  const termId = router.query.termId as string;
+  const termName = getTermName(termInfos, termId).replace('Semester', '');
+
+  return (
+    <>
+      <div className="Empty_Container">
+        <div className="Empty_MainWrapper">
+          <div className="Empty_Main">
+            <div className="Empty_Main__EmptyCard">
+              <div className="Empty_Main_EmptyCard_Header">
+                <div className="Empty_Main__EmptyCard_Header_Spacer">
+                  <div className="Empty_Main__EmptyCard_Header_Title">
+                    <b>{termName} Notifications</b>
+                  </div>
+                </div>
+                {isHovering ? (
+                  <div className="Happy_Husky_SVG">
+                    <HappyHusky />
+                  </div>
+                ) : (
+                  <div className="Crying_Husky_SVG">
+                    <CryingHusky />
+                  </div>
+                )}
+              </div>
+              <ClassCardWrapper
+                headerLeft={
+                  <div className="Empty_Main__EmptyCard_Text">
+                    <div className="Empty_Main__EmptyCard_Text_Title">
+                      <b>You currently have no notifications. Hoosky sad :(</b>
+                    </div>
+                    <div className="Empty_Main__EmptyCard_Text_Body">
+                      Be the first to know when new classes and sections drop!
+                    </div>
+                  </div>
+                }
+                headerRight={
+                  <div
+                    className="Empty_Main__EmptyCard_Divider"
+                    onClick={() => {
+                      router.push(`/NEU/${termId}/search`);
+                    }}
+                    onMouseEnter={() => setIsHovering(true)}
+                    onMouseLeave={() => setIsHovering(false)}
+                  >
+                    <button className="Empty_Main__EmptyCard_Button">
+                      <Circular />
+                      <b>Search for classes</b>
+                    </button>
+                  </div>
+                }
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};

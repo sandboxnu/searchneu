deb1f2bc30f14fee9a5d2ba9910d7a37a90720bf
cherman27 <carterherman27@gmail.com>
2025-02-27 11:06:56 -0500

    Mobile Header semester dropdwown style fix

--- components/ResultsPage/MobileDropdown.tsx
+++ components/ResultsPage/MobileDropdown.tsx
@@ -4,6 +4,7 @@ import { Dropdown } from 'semantic-ui-react';
 import DropdownArrow from '../icons/DropdownArrow.svg';
 import { getTermName, TermInfo } from '../terms';
 import { Campus } from '../types';
+import { NodeNextRequest } from 'next/dist/server/base-http/node';
 
 interface ItemProps {
   text: string;
@@ -22,28 +23,40 @@ function MobileSemesterDropdown({
   termInfos,
 }: DropdownProps): ReactElement {
   const currSemester = getTermName(termInfos, currentTerm).split(' ');
+
+  // This is crazy lmao
   const semester =
-    currSemester.length == 3
+    currSemester.length === 3
       ? currSemester[0]
       : currSemester[0] + ' ' + currSemester[1];
-  const currYear = currSemester.length == 3 ? currSemester[1] : currSemester[2];
-  const currText = semester + ' (' + currYear + ')';
+  const currYear =
+    currSemester.length === 3 ? currSemester[1] : currSemester[2];
+  const currText = `${semester} (${currYear})`;
+
   return (
     <Dropdown
       fluid
-      text={currText}
+      trigger={
+        <div className="MobileDropdown__Trigger">
+          <span>{currText}</span>
+          <DropdownArrow className="MobileDropdown__Arrow" />
+        </div>
+      }
+      icon={null}
       className="MobileDropdown"
-      icon={DropdownArrow}
     >
       <Dropdown.Menu className="MobileDropdown__Menu">
         {options.map(({ text, value, link }) => (
           <Link key={value} href={link}>
-            <Dropdown.Item selected={currentTerm == value} text={text} />
+            <Dropdown.Item
+              className="MobileDropdown__Item"
+              selected={currentTerm === value}
+              text={text}
+            />
           </Link>
         ))}
       </Dropdown.Menu>
     </Dropdown>
   );
 }
-
 export default React.memo(MobileSemesterDropdown);

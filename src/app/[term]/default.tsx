f1906e066bf5dd372bc1e22380c7b9d6c707448b
mehallhm <mehall.m@northeastern.edu>
2025-04-08 13:44:41 -0400

    more build errors

--- src/app/[term]/default.tsx
+++ src/app/[term]/default.tsx
@@ -1,8 +1,5 @@
 import Page from "./page";
 
-export default function Default(props: {
-  params: Promise<{ term: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
-  return <Page params={props.params} searchParams={props.searchParams} />;
+export default function Default() {
+  return <Page />;
 }

1b3b3d4b7ab68a6b3e3c33ed7467de6be87ea7a0
mehallhm <mehall.m@northeastern.edu>
2025-05-06 02:00:17 -0600

    controller cleanup

--- src/app/(app)/catalog/[term]/layout.tsx
+++ src/app/(app)/catalog/[term]/layout.tsx
@@ -1,20 +1,38 @@
 import { type ReactNode } from "react";
 import { getTerms } from "@/lib/controllers/getTerms";
-import { getSubjects } from "@/lib/controllers/getSubjects";
 import { MobileWrapper } from "@/components/search/MobileWrapper";
-import { getCampuses } from "@/lib/controllers/getCampuses";
-import { getClassTypes } from "@/lib/controllers/getClassTypes";
 import type { Option } from "@/components/ui/multi-select";
 import { NUPATH_OPTIONS } from "@/lib/banner/nupaths";
+import { db } from "@/db";
+import { subjectsT, sectionsT } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export default async function Layout(props: {
   params: Promise<{ term: string; course?: string }>;
   children: ReactNode;
 }) {
   const terms = getTerms();
-  const subjects = getSubjects((await props.params)?.term ?? "");
-  const campuses = getCampuses((await props.params)?.term ?? "");
-  const classTypes = getClassTypes((await props.params)?.term ?? "");
+
+  const subjects = db
+    .select({
+      value: subjectsT.code,
+      label: subjectsT.name,
+    })
+    .from(subjectsT)
+    .where(eq(subjectsT.term, (await props.params)?.term ?? ""));
+
+  const campuses = db
+    .selectDistinct({ campus: sectionsT.campus })
+    .from(sectionsT)
+    .where(eq(sectionsT.term, (await props.params)?.term ?? ""))
+    .then((c) => c.map((e) => e.campus));
+
+  const classTypes = db
+    .selectDistinct({ classType: sectionsT.classType })
+    .from(sectionsT)
+    .where(eq(sectionsT.term, (await props.params)?.term ?? ""))
+    .then((c) => c.map((e) => e.classType));
+
   // NOTE: the static nupaths needs a promise b/c the use hook is called on it later
   const nupaths = new Promise((r) => r(NUPATH_OPTIONS)) as Promise<Option[]>;
 

5acb87f625d4bddff49a123d48151a797ff04be5
mehallhm <mehall.m@northeastern.edu>
2025-05-09 16:57:48 -0600

    debugging for scalar client

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -4,11 +4,29 @@ import { APIPage } from "fumadocs-openapi/ui";
 import { openapi } from "@/lib/source";
 import { Popup, PopupContent, PopupTrigger } from "fumadocs-twoslash/ui";
 import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
+import { readFileSync } from "fs";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
+    APIPage: (props) => {
+      const oapiProps = openapi.getAPIPageProps(props);
+
+      const path = oapiProps.document;
+
+      const file = readFileSync(path);
+      const size = file.byteLength;
+
+      console.log(size);
+
+      return (
+        <div>
+          <pre>size: {size}B</pre>
+          <pre>{JSON.stringify(oapiProps, null, 2)}</pre>
+          <APIPage {...oapiProps} />
+        </div>
+      );
+    },
     Popup,
     PopupContent,
     PopupTrigger,

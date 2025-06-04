899628141eeddde0284d77f8052c52dde5ab7e27
mehallhm <mehall.m@northeastern.edu>
2025-06-03 19:41:51 -0600

    remove unecessary div

--- src/app/(app)/catalog/layout.tsx
+++ src/app/(app)/catalog/layout.tsx
@@ -3,9 +3,9 @@ import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-screen">
+    <>
       <Header />
       <div className="">{children}</div>
-    </div>
+    </>
   );
 }

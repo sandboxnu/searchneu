c2ff17d386ff14e885e28548744ed7bcdf5fbd7e
mehallhm <mehall.m@northeastern.edu>
2025-06-04 21:50:39 -0600

    remove unecessary twilio import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
-import { Twilio } from "twilio";
 import * as schema from "./schema";
 
 export const db = drizzle({
   connection: process.env.DATABASE_URL!,
   schema: schema,
 });
-
-export const twilio = new Twilio(
-  process.env.TWILIO_SID,
-  process.env.TWILIO_AUTH_TOKEN,
-);

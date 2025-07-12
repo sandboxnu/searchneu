637977aa34797f1092cb8d1b704b6056ad782290
mehallhm <mehall.m@northeastern.edu>
2025-07-12 16:54:54 -0600

    fix: banner api docs generation for prod env

--- src/scripts/generate-docs.mjs
+++ src/scripts/generate-docs.mjs
@@ -3,7 +3,9 @@ import { generateFiles } from "fumadocs-openapi";
 void main().catch(console.error);
 
 async function main() {
-  const input = ["./content/api/banner-openapi.yaml"];
+  const input = [
+    "https://raw.githubusercontent.com/sandboxnu/search2/refs/heads/main/content/api/banner-openapi.yaml",
+  ];
   const output = "./content/docs/banner-api";
 
   await generateFiles({
@@ -11,6 +13,6 @@ async function main() {
     output: output,
     includeDescription: true,
     per: "operation",
-    groupBy: "none",
+    groupBy: "tag",
   });
 }

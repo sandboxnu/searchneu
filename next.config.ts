6cf80118eff4c450366ed2f50bbf10fe419105a4
mehallhm <mehall.m@northeastern.edu>
2025-07-12 10:20:28 -0600

    fix: include openapi specs in bundling

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  outputFileTracingIncludes: {
+    "/content/api/": ["./content/api/*"],
+  },
 };
 
 const vercelToolbar = withVercelToolbar();

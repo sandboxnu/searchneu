da516fa209156ce80e94ffe64edecace982822bf
aaryan1203 <aaryanjain1203@gmail.com>
2025-09-21 16:50:27 -0400

    Fixed comments

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -5,14 +5,14 @@ const isDev = !process.env.IS_PROD;
 
 // TODO: Adding pino pretty target causes error
 const logger = pino({
-  // transport: !isDev
-  //   ? undefined
-  //   : {
+  // transport: isDev
+  //   ? {
   //       target: "pino-pretty",
   //       options: {
   //         colorize: true,
   //       },
-  //     },
+  //     }
+  //   : undefined,
   timestamp: () => `,"time":"${new Date().toLocaleString()}"`,
   level: process.env.LOG_LEVEL || "info",
 });

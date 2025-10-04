58b13581f9e3de8031cb6c9a1130a0c6e4722c0a
Sarah-Zhang1 <zhang.sarah1@northeastern.edu>
2025-10-04 11:59:23 -0400

    add flag to page instead

--- src/app/(app)/scheduler/page.tsx
+++ src/app/(app)/scheduler/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <p>TODO</p>;
+import { Header } from "@/components/navigation/Header";
+import { schedulerFlag } from "@/lib/flags";
+import { notFound } from "next/navigation";
+
+export default async function Page({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const schedulerPage = await schedulerFlag();
+
+  if (!schedulerPage) {
+    notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 }

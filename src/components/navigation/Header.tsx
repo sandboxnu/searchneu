39146d5d2b9fe7882654a35dd464d3e0d383c84e
aaryan1203 <aaryanjain1203@gmail.com>
2025-10-15 23:53:05 -0400

    Updated header designs

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Logo } from "../icons/logo";
 import { UserIcon } from "./UserMenu";
 import { faqFlag, roomsFlag } from "@/lib/flags";
+import { Bookmark, CircleQuestionMark } from "lucide-react"; // 👈 Add icons
 
 export async function Header() {
   const enableFaqPage = await faqFlag();
@@ -14,24 +15,29 @@ export async function Header() {
         <Logo className="h-6 w-40" />
       </Link>
       <div className="flex items-center gap-4">
-        <nav className="space-x-2 font-semibold">
-          <Link
-            href="/catalog"
-            className="hover:bg-neu3 bg-background rounded-full border-1 px-4 py-2"
-          >
-            Catalog
-          </Link>
+        <nav className="flex gap-2 font-semibold">
           {enableRoomsPage && (
             <Link
               href="/rooms"
-              className="hover:bg-neu3 bg-background rounded-full border-1 px-4 py-2"
+              className="hover:bg-neu3 bg-background flex items-center gap-2 rounded-full border-1 px-4 py-2"
             >
-              Rooms
+              <Bookmark className="h-6" />
+              <span>Rooms</span>
             </Link>
           )}
+          <Link
+            href="/catalog"
+            className="hover:bg-neu3 bg-background flex items-center gap-2 rounded-full border-1 px-4 py-2 leading-none"
+          >
+            <Bookmark className="h-6" />
+            <span>Catalog</span>
+          </Link>
           {enableFaqPage && (
-            <Link href="/faq" className="hover:bg-neu3 rounded-lg p-2">
-              FAQ
+            <Link
+              href="/faq"
+              className="hover:bg-neu3 bg-background flex items-center gap-2 rounded-full border-1 px-2 py-2"
+            >
+              <CircleQuestionMark color="red" />
             </Link>
           )}
         </nav>

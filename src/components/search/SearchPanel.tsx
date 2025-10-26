4fe5392633844d8bde8f7cee9ff949cfdad50dc9
Aidan Szeto <szeto.ai@northeastern.edu>
2025-10-26 14:51:32 -0400

    removed error line

--- src/components/search/SearchPanel.tsx
+++ src/components/search/SearchPanel.tsx
@@ -73,7 +73,6 @@ export function SearchPanel(props: {
             transform={(opts) => opts.map((c) => ({ value: c, label: c }))}
           />
         </Suspense>
-        <p>{props.}</p>
       </div>
 
       <Separator />

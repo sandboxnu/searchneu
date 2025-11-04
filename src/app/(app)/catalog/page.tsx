0e97cbd78ff96b99910add9c60b3965fa7611248
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2025-11-03 19:47:32 -0500

    test

--- src/app/(app)/catalog/page.tsx
+++ src/app/(app)/catalog/page.tsx
@@ -1,6 +1,8 @@
 import { getTerms } from "@/lib/controllers/getTerms";
 import { redirect } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const terms = await getTerms();
   redirect(`/catalog/${terms.neu[0].term}`);

b62a76132582bac99beaaa9ac9fb172610a1691e
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2025-11-03 19:53:45 -0500

    try build with skip env validation

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,8 +6,6 @@ import type { Metadata } from "next";
 import { Search } from "@/components/home/Search";
 import ClickSpark from "@/components/ui/click-spark";
 
-export const dynamic = 'force-dynamic';
-
 export const metadata: Metadata = {
   title: "SearchNEU",
   description: "Search courses at Northeastern University",

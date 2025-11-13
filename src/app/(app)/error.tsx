2f3924c0e642ca7e80e9b347fa9e40dee3f22111
Lucas Johnson <johnson.luca@northeastern.edu>
2025-11-13 17:14:12 -0500

    finished first draft of ticket

--- src/app/(app)/error.tsx
+++ src/app/(app)/error.tsx
@@ -1,5 +1,15 @@
 "use client";
 
-export default function Error() {
-  return <p>Ooops...</p>;
+import {useEffect} from "react"
+import {logger} from "@/lib/logger"
+
+export default function Error({
+  error,
+} : {error: Error & {digest?: string}
+}) {
+  useEffect(() => {
+    logger.error(error)
+  }, [error])
+
+  return <p>Something broke DDD:</p>;
 }
---
+++ src/components/navigation/NavError.tsx
@@ -0,0 +1,7 @@
+export function NavErrorFallback({ error }: {error: Error}) {
+    return (
+      <div className="text-red-600">
+        <h2>????</h2>
+      </div>
+    );
+  }

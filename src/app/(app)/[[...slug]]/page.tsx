0b44ec3e7850089fe89c968d1760156824ed5eb4
AidanS99910 <szeto.aidan@doversherborn.org>
2025-11-25 16:05:56 -0500

    added note

--- src/app/(app)/[[...slug]]/page.tsx
+++ src/app/(app)/[[...slug]]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation'
 
+// NOTE: this and the slug folder will cause npm run dev to not work. delete/move out of (app) remporarily to solve.
 export default function CatchAll() {
   notFound()
 }

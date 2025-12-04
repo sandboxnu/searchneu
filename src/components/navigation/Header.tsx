2902fef42d4d790abd5a207a45abe3fe574ce4f8
mehallhm <mehall.m@northeastern.edu>
2025-12-04 15:52:10 -0500

    feat: Banner change notif outage header

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -13,6 +13,7 @@ import {
 } from "../ui/sheet";
 import { Button } from "../ui/button";
 import { NavBar } from "./NavBar";
+import { BannerWarning } from "./BannerWarning";
 
 export function Header() {
   const enableFaqPage = faqFlag();
@@ -32,39 +33,42 @@ export function Header() {
   );
 
   return (
-    <header className="z-20 flex h-9 w-full items-center justify-between bg-transparent px-4 md:px-6">
-      <Link href="/">
-        <Logo className="h-6 w-40" />
-      </Link>
-      <div className="hidden items-center gap-2 lg:flex">
-        {Nav}
-        <UserIcon />
-      </div>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="bg-neu1 rounded-full border lg:hidden"
-          >
-            <MenuIcon className="text-neu8 size-5" />
-          </Button>
-        </SheetTrigger>
-        <SheetContent className="bg-neu2 flex w-[90%] flex-col justify-between px-6 pt-6 pb-4">
-          <SheetTitle className="hidden">Nav bar</SheetTitle>
-          <div className="flex flex-col gap-8">
-            <Link href="/">
-              <Logo className="h-6 w-40" />
-            </Link>
-            <UserIcon />
-            {Nav}
-          </div>
-          <SheetClose className="bg-neu1 absolute top-4 right-4 flex size-9 items-center justify-center rounded-full border disabled:pointer-events-none">
-            <XIcon className="size-6" />
-            <span className="sr-only">Close</span>
-          </SheetClose>
-        </SheetContent>
-      </Sheet>
-    </header>
+    <>
+      <BannerWarning />
+      <header className="z-20 flex h-9 w-full items-center justify-between bg-transparent px-4 md:px-6">
+        <Link href="/">
+          <Logo className="h-6 w-40" />
+        </Link>
+        <div className="hidden items-center gap-2 lg:flex">
+          {Nav}
+          <UserIcon />
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="bg-neu1 rounded-full border lg:hidden"
+            >
+              <MenuIcon className="text-neu8 size-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="bg-neu2 flex w-[90%] flex-col justify-between px-6 pt-6 pb-4">
+            <SheetTitle className="hidden">Nav bar</SheetTitle>
+            <div className="flex flex-col gap-8">
+              <Link href="/">
+                <Logo className="h-6 w-40" />
+              </Link>
+              <UserIcon />
+              {Nav}
+            </div>
+            <SheetClose className="bg-neu1 absolute top-4 right-4 flex size-9 items-center justify-center rounded-full border disabled:pointer-events-none">
+              <XIcon className="size-6" />
+              <span className="sr-only">Close</span>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
+      </header>
+    </>
   );
 }

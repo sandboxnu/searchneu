715d27f4f426b78c575d53a5aa9ec557fe333318
mehallhm <mehall.m@northeastern.edu>
2025-12-04 15:22:50 -0500

    remove pino logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,14 @@
-import pino from "pino";
-
-export const logger = pino({
-  level: process.env.LOG_LEVEL ?? "info",
-  timestamp: pino.stdTimeFunctions.isoTime,
-  base: null,
-});
+export const logger = {
+  debug(message?: any, ...optionalParams: any[]) {
+    console.debug(message, ...optionalParams);
+  },
+  info(message?: any, ...optionalParams: any[]) {
+    console.info(message, ...optionalParams);
+  },
+  warn(message?: any, ...optionalParams: any[]) {
+    console.warn(message, ...optionalParams);
+  },
+  error(message?: any, ...optionalParams: any[]) {
+    console.error(message, ...optionalParams);
+  },
+};

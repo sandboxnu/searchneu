ac8be92af7c9a8a0acc716f498b5740a034d74ba
sanjana-singhania <singhania.sa@northeastern.edu>
2025-12-10 16:22:34 -0500

    import statement

--- src/lib/scheduler/binaryMeetingTimeTests/binaryMeetingTime.test.ts
+++ src/lib/scheduler/binaryMeetingTimeTests/binaryMeetingTime.test.ts
@@ -1,4 +1,4 @@
-import test, { describe } from "node:test";
+import { describe, test } from "node:test";
 import assert from "node:assert/strict";
 
 import { hasConflictInSchedule } from "../binaryMeetingTime";

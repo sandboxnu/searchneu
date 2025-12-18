c5a08accc3a6342d8de2afafdb4d2193309c0f3e
Lucas Johnson <johnson.luca@northeastern.edu>
2025-12-18 15:39:55 -0800

    Finished groundwork for documentation app

--- apps/docs/app/(home)/[[...slug]]/page.tsx
+++ apps/docs/app/(home)/[[...slug]]/page.tsx
@@ -10,7 +10,7 @@ import { getMDXComponents } from "@/mdx-components";
 import type { Metadata } from "next";
 import { createRelativeLink } from "fumadocs-ui/mdx";
 
-export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
+export default async function Page(props: PageProps<"/[[...slug]]">) {
   const params = await props.params;
   const page = source.getPage(params.slug);
   if (!page) notFound();
@@ -38,7 +38,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata(
-  props: PageProps<"/docs/[[...slug]]">,
+  props: PageProps<"/[[...slug]]">,
 ): Promise<Metadata> {
   const params = await props.params;
   const page = source.getPage(params.slug);
--- apps/docs/app/og/[...slug]/route.tsx
+++ apps/docs/app/og/[...slug]/route.tsx
@@ -7,7 +7,7 @@ export const revalidate = false;
 
 export async function GET(
   _req: Request,
-  { params }: RouteContext<"/og/docs/[...slug]">,
+  { params }: RouteContext<"/og/[...slug]">,
 ) {
   const { slug } = await params;
   const page = source.getPage(slug.slice(0, -1));

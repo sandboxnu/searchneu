8efbf7bd4f9ec4ad4c6d08bcc8fb987d0a2542aa
ShireenKumar <shireen.kumar1304@gmail.com>
2026-02-03 16:19:06 -0500

    Range Validation tests

--- apps/searchneu/lib/graduate/validation-worker/unit-validation.test.ts
+++ apps/searchneu/lib/graduate/validation-worker/unit-validation.test.ts
@@ -1,6 +1,6 @@
 import { test, describe } from "node:test";
 import assert from "node:assert";
-import { makeTracker, range, solution } from "./test-utils";
+import { makeTracker, range, solution, xom } from "./test-utils";
 import { CourseError, validateRequirement } from "./major2-validation";
 import * as c from "./mock-courses";
 import { Err, Ok } from "../types";
@@ -16,10 +16,26 @@ describe("validate range requirement", () => {
   });
   test("range requirement fail", () => {
     const rangeRequirement = range(8, "CS", 2000, 4000, [c.cs2500])
-    const tracker = makeTracker(c.cs2800, c.cs2810, c.cs3500);
+    const tracker = makeTracker(c.cs1200, c.cs4300, c.cs3500);
     console.log(validateRequirement(rangeRequirement, tracker));
     assert.deepStrictEqual(validateRequirement(rangeRequirement, tracker),
-      Err(CourseError(c.cs2500)),
+    Ok([solution(c.cs3500)]),
     );
   })
 });
+
+test("XOM with range - insufficient credits", () => {
+  const xomRequirement = xom(12, [range(12, "CS", 2000, 4000, [])]);
+  
+  const tracker = makeTracker(c.cs2800, c.cs3500); 
+  
+  const result = validateRequirement(xomRequirement, tracker);
+  
+  assert.strictEqual(result.type, "Err");
+  if (result.type === "Err") {
+    assert.strictEqual(result.err.type, "XOM");
+    assert.strictEqual(result.err.minRequiredCredits, 12);
+    assert.strictEqual(result.err.maxPossibleCredits, 8);
+  }
+});
+

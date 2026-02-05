b4e1a262b97f0de2474acf18a0698f2d14079675
mehallhm <mehall.m@northeastern.edu>
2026-02-05 13:50:40 -0500

    feat: repository tooling
    Tooling across the monorepo to improve developer experience, code
    conformity, and styling. Sets up ESLint for all packages with shared
    config for code linting and analysis. Configures workspace-wide Prettier
    for unified code styling. Reconfigures turbo to pull package specific
    commands into package configurations. Added additional github action for
    code linting and formatting. Centralizes additional TSConfig settings.

--- apps/searchneu/eslint.config.mjs
+++ apps/searchneu/eslint.config.mjs
@@ -1,16 +1,4 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { config } from "@sneu/eslint-config/next-js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+/** @type {import("eslint").Linter.Config} */
+export default [...config];

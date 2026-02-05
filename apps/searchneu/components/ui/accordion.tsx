eb86eae548d900427c25823bf80a494cc266faf8
Emma Fang <fang.emm@northeastern.edu>
2026-02-05 00:12:49 -0500

    header changes color on accordion opening, content is animated smoothly (text fades in and out, smooth accordion down and up)

--- apps/searchneu/components/ui/accordion.tsx
+++ apps/searchneu/components/ui/accordion.tsx
@@ -13,17 +13,17 @@ export default function FAQDropDown({title, description}: AccordionType) {
   return(
 	<div className="rounded-lg gap-[16px] self-stretch flex flex-col items-start p-[24px] bg-[#FFFFFF]">
 		<Accordion.Root type="single" collapsible className="w-full">
-			<Accordion.Item value="item1" className="data-[state=open]:animate-in data-[state=closed]:animate-out">
+			<Accordion.Item value="item1">
 				<Accordion.Header>
-					<Accordion.Trigger className="flex items-center justify-between w-full data-[state=open]:[&>span]:duration-300 data-[state=open]:[&>span]:rotate-180 data-[state=closed]:[&>span]:duration-300 data-[state=closed]:[&>span]:rotate-0"> 
-					<h4 className="text-xl font-medium text-neu7;">{title}</h4> 
+					<Accordion.Trigger className="flex items-center justify-between w-full data-[state=closed]:text-neu7 data-[state=open]:text-neu8 data-[state=open]:[&>span]:duration-300 data-[state=open]:[&>span]:rotate-180 data-[state=closed]:[&>span]:duration-300 data-[state=closed]:[&>span]:rotate-0"> 
+					<h4 className="text-xl font-medium;">{title}</h4> 
 					<span>
 						<ChevronDownIcon color="#C2C2C2"/>
 					</span>
 					</Accordion.Trigger>
 				</Accordion.Header>
-				<Accordion.Content> 
-					<p className="text-base"> {description} </p>
+				<Accordion.Content className="data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up data-[state=open]:animate-in data-[state=closed]:animate-out fade-in fade-out"> 
+					<p className="text-base text-neu7"> {description} </p>
 				</Accordion.Content>
 			</Accordion.Item>
 		</Accordion.Root>

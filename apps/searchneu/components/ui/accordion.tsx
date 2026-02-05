5b57a3d433f1ca2ef629b004662315d7ad07881f
Emma Fang <fang.emm@northeastern.edu>
2026-02-04 23:38:08 -0500

    chevron icon now rotates smoothly :D

--- apps/searchneu/components/ui/accordion.tsx
+++ apps/searchneu/components/ui/accordion.tsx
@@ -13,11 +13,13 @@ export default function FAQDropDown({title, description}: AccordionType) {
   return(
 	<div className="rounded-lg gap-[16px] self-stretch flex flex-col items-start p-[24px] bg-[#FFFFFF]">
 		<Accordion.Root type="single" collapsible className="w-full">
-			<Accordion.Item value="item1">
+			<Accordion.Item value="item1" className="data-[state=open]:animate-in data-[state=closed]:animate-out">
 				<Accordion.Header>
-					<Accordion.Trigger className="flex items-center justify-between w-full"> 
-					<h4 className="text-xl font-medium text-[#5F5F5F];">{title}</h4> 
-					<ChevronDownIcon color="#C2C2C2"/>
+					<Accordion.Trigger className="flex items-center justify-between w-full data-[state=open]:[&>span]:duration-300 data-[state=open]:[&>span]:rotate-180 data-[state=closed]:[&>span]:duration-300 data-[state=closed]:[&>span]:rotate-0"> 
+					<h4 className="text-xl font-medium text-neu7;">{title}</h4> 
+					<span>
+						<ChevronDownIcon color="#C2C2C2"/>
+					</span>
 					</Accordion.Trigger>
 				</Accordion.Header>
 				<Accordion.Content> 

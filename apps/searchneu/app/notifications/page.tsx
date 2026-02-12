67673c8bf6e44b654af77bf598511274f8feec6a
aaryan1203 <aaryanjain1203@gmail.com>
2026-02-12 16:39:03 -0500

    Initial sql query

--- apps/searchneu/app/notifications/page.tsx
+++ apps/searchneu/app/notifications/page.tsx
@@ -1,6 +1,35 @@
+import { SectionTableMeetingTime } from "@/components/catalog/SectionTable";
 import { NotificationsWrapper } from "@/components/notifications/NotificationsWrapper";
+import { db } from "@/lib/db";
+import { notificationsT } from "@sneu/db/schema";
+import { getTrackedSections } from "../catalog/[term]/[course]/page";
+import { inArray } from "drizzle-orm";
+import { getSectionInfo } from "@/lib/controllers/getTrackers";
 
-export default function Page() {
+export interface TrackerSection {
+  id: number;
+  crn: string;
+  faculty: string;
+  meetingTimes: SectionTableMeetingTime[];
+  campus: string;
+  seatRemaining: number;
+  seatCapacity: number;
+  waitlistCapacity: number;
+  waitlistRemaining: number;
+}
+
+export default async function Page() {
+  const trackedSections = await getTrackedSections();
+  const notifications = await db.query.notificationsT.findMany({
+    where: inArray(notificationsT.trackerId, trackedSections),
+  });
+  console.log("Tracked sections", trackedSections);
+  console.log("Notifcaitons", notifications);
+
+  const sections = await getSectionInfo(trackedSections);
+  console.log("SECTION", sections);
+  console.log("SECTION FIRST", sections);
+
   return (
     <div className="bg-secondary h-full min-h-0 w-full overflow-hidden px-4 xl:px-6">
       <NotificationsWrapper />

93259a0b4f345e2e5f4306608b43c9860749d411
ArmaanMehra <mehra.ar@northeastern.edu>
2026-02-21 05:15:05 -0500

    fixed prettier formatting

--- apps/searchneu/app/scheduler/layout.tsx
+++ apps/searchneu/app/scheduler/layout.tsx
@@ -7,7 +7,7 @@ export default async function Layout({ children }: LayoutProps<"/rooms">) {
   return (
     <div className="flex h-screen flex-col pt-4">
       <Header />
-      <div className="flex-1 min-h-0">
+      <div className="min-h-0 flex-1">
         <Suspense>
           <FlagCheck>{children}</FlagCheck>
         </Suspense>

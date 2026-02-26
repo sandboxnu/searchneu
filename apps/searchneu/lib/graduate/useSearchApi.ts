8465f389ad2964e2f302b15c9e8c2e2de6b0554f
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2026-02-26 18:02:45 -0500

    map to cache course info

--- apps/searchneu/lib/graduate/useSearchApi.ts
+++ apps/searchneu/lib/graduate/useSearchApi.ts
@@ -1,9 +1,9 @@
 "use client"
+import { Course } from "@sneu/scraper/types";
 import { useState, useEffect } from "react";
 
-export function useSearchCourse(term:string,subject:string, courseNumber:number) {
-  // pulled from resultcard.tsx
-  const [data, setData] = useState<{
+// DENNIS HELP : do we have a shared type????
+interface CourseInfo {
     name: string;
     courseNumber: string;
     subject: string;
@@ -12,14 +12,24 @@ export function useSearchCourse(term:string,subject:string, courseNumber:number)
     sectionsWithSeats: number;
     totalSections: number;
     nupaths: string[];
-  } | null>(null);
+};
+
+const cache = new Map<string, CourseInfo>();
+
+export function useSearchCourse(term:string,subject:string, courseNumber:number) {
+  const key = `${term}-${subject}-${courseNumber}`;
+  const [data, setData] = useState<CourseInfo | null>(cache.get(key) ?? null);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
+    if (cache.has(key)) {
+      return;
+    }
     const fetchMajors = async () => {
       try {
         const res = await (await fetch(`/api/search?term=${term}&subj=${subject}&q=${courseNumber}`))
         const json = await res.json()
+        cache.set(key, json[0])
         setData(json[0]);
       } catch (err) {
         setError(

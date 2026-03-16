a199209508eaba715db24cb440f5e12112b90f48
mehallhm <mehall.m@northeastern.edu>
2026-03-16 15:26:42 -0400

    feat: launch faq page

--- apps/searchneu/app/faq/layout.tsx
+++ apps/searchneu/app/faq/layout.tsx
@@ -1,25 +1,11 @@
 import { Header } from "@/components/navigation/Header";
-import { faqFlag } from "@/lib/flags";
-import { notFound } from "next/navigation";
-import { Suspense, type ReactNode } from "react";
+import { Suspense } from "react";
 
-export default async function Layout({ children }: LayoutProps<"/faq">) {
+export default function Layout({ children }: LayoutProps<"/faq">) {
   return (
     <div className="pt-4">
       <Header />
-      <Suspense>
-        <FlagCheck>{children}</FlagCheck>
-      </Suspense>
+      <Suspense>{children}</Suspense>
     </div>
   );
-}
-
-async function FlagCheck({ children }: { children: ReactNode }) {
-  const faqPage = await faqFlag();
-
-  if (!faqPage) {
-    notFound();
-  }
-
-  return children;
 }
--- apps/searchneu/app/notifications/layout.tsx
+++ apps/searchneu/app/notifications/layout.tsx
@@ -1,27 +1,11 @@
 import { Header } from "@/components/navigation/Header";
-import { notificationsFlag } from "@/lib/flags";
-import { notFound } from "next/navigation";
-import { Suspense, type ReactNode } from "react";
+import { Suspense } from "react";
 
-export default async function Layout({
-  children,
-}: LayoutProps<"/notifications">) {
+export default function Layout({ children }: LayoutProps<"/notifications">) {
   return (
     <div className="flex h-dvh flex-col pt-4">
       <Header />
-      <Suspense>
-        <FlagCheck>{children}</FlagCheck>
-      </Suspense>
+      <Suspense>{children}</Suspense>
     </div>
   );
-}
-
-async function FlagCheck({ children }: { children: ReactNode }) {
-  const notificationsPage = await notificationsFlag();
-
-  if (!notificationsPage) {
-    notFound();
-  }
-
-  return children;
 }

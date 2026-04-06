95e06334b1da1d98c5bac15879a866cdc933b553
aaryan1203 <aaryanjain1203@gmail.com>
2026-04-05 22:11:55 -0400

    Added giving day toast

--- apps/searchneu/components/ClientLayout.tsx
+++ apps/searchneu/components/ClientLayout.tsx
@@ -2,34 +2,63 @@
 
 import FeedbackModal from "@/components/feedback/FeedbackModal";
 import { FeedbackProvider } from "@/components/feedback/FeedbackContext";
-import dynamic from "next/dynamic";
+import { X } from "lucide-react";
+import { useEffect } from "react";
+import { HappyHusky } from "./icons/HappyHusky";
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/toaster";
+// import dynamic from "next/dynamic";
 
-const GivingDayModal = dynamic(() => import("./GivingDayModal"), {
-  ssr: false,
-});
+// const GivingDayModal = dynamic(() => import("./GivingDayModal"), {
+//   ssr: false,
+// });
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  // useEffect(() => {
-  //   if (!sessionStorage.getItem("summer-delay-shown")) {
-  //     toast("Summer course catalog delayed :(", {
-  //       description: "We are working to get them to you soon.",
-  //       position: "bottom-left",
-  //       icon: <SadHusky />,
-  //       cancel: {
-  //         label: <X size={20} color="#F15B50" className="h-[16px] w-[16px]" />,
-  //         onClick: () => {},
-  //       },
-  //     });
-  //     sessionStorage.setItem("summer-delay-shown", "true");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (!sessionStorage.getItem("summer-delay-shown")) {
+      toast("Giving Day is coming up on April 10th!", {
+        description: (
+          <>
+            Please donate to Sandbox{" "}
+            <a
+              href="http://tinyurl.com/sandbox-giving-day"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "underline" }}
+            >
+              here
+            </a>{" "}
+            to help keep SearchNEU running!
+          </>
+        ),
+        position: "bottom-left",
+        icon: <HappyHusky />,
+        cancel: {
+          label: <X size={20} color="#F15B50" className="h-[16px] w-[16px]" />,
+          onClick: () => {},
+        },
+      });
+      sessionStorage.setItem("summer-delay-shown", "true");
+    }
+  }, []);
 
   return (
     <FeedbackProvider>
       <div>
         <main className="min-h-[100dvh] w-screen grow">{children}</main>
         <FeedbackModal />
-        <GivingDayModal />
+        <Toaster
+          toastOptions={{
+            classNames: {
+              toast:
+                "!border-l-8 !border-l-r4 !text-[13px] !w-[450px] !h-[64px]",
+              cancelButton: "!bg-white !p-0",
+              icon: "!w-[35.77px] !h-[32.95px]",
+              description: "!text-[11px] !-mt-1",
+            },
+          }}
+        />
+        {/* <GivingDayModal /> */}
       </div>
     </FeedbackProvider>
   );

cee09aeac354fc03d483951102958f9f0ee9e134
aaryan1203 <aaryanjain1203@gmail.com>
2026-04-13 12:45:08 -0400

    Giving day modal

--- apps/searchneu/components/ClientLayout.tsx
+++ apps/searchneu/components/ClientLayout.tsx
@@ -2,52 +2,47 @@
 
 import FeedbackModal from "@/components/feedback/FeedbackModal";
 import { FeedbackProvider } from "@/components/feedback/FeedbackContext";
-import { X } from "lucide-react";
-import { useEffect } from "react";
-import { HappyHusky } from "./icons/HappyHusky";
-import { toast } from "sonner";
-import { Toaster } from "@/components/ui/toaster";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 
-// const GivingDayModal = dynamic(() => import("./GivingDayModal"), {
-//   ssr: false,
-// });
+const GivingDayModal = dynamic(() => import("./GivingDayModal"), {
+  ssr: false,
+});
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    if (!sessionStorage.getItem("summer-delay-shown")) {
-      toast("Giving Day is coming up on April 10th!", {
-        description: (
-          <>
-            Please donate to Sandbox{" "}
-            <a
-              href="http://tinyurl.com/sandbox-giving-day"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "underline" }}
-            >
-              here
-            </a>{" "}
-            to help keep SearchNEU running!
-          </>
-        ),
-        position: "bottom-left",
-        icon: <HappyHusky />,
-        cancel: {
-          label: <X size={20} color="#F15B50" className="h-[16px] w-[16px]" />,
-          onClick: () => {},
-        },
-      });
-      sessionStorage.setItem("summer-delay-shown", "true");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (!sessionStorage.getItem("summer-delay-shown")) {
+  //     toast("Giving Day is coming up on April 10th!", {
+  //       description: (
+  //         <>
+  //           Please donate to Sandbox{" "}
+  //           <a
+  //             href="http://tinyurl.com/sandbox-giving-day"
+  //             target="_blank"
+  //             rel="noopener noreferrer"
+  //             style={{ textDecoration: "underline" }}
+  //           >
+  //             here
+  //           </a>{" "}
+  //           to help keep SearchNEU running!
+  //         </>
+  //       ),
+  //       position: "bottom-left",
+  //       icon: <HappyHusky />,
+  //       cancel: {
+  //         label: <X size={20} color="#F15B50" className="h-[16px] w-[16px]" />,
+  //         onClick: () => {},
+  //       },
+  //     });
+  //     sessionStorage.setItem("summer-delay-shown", "true");
+  //   }
+  // }, []);
 
   return (
     <FeedbackProvider>
       <div>
         <main className="min-h-[100dvh] w-screen grow">{children}</main>
         <FeedbackModal />
-        <Toaster
+        {/* <Toaster
           toastOptions={{
             classNames: {
               toast:
@@ -57,8 +52,8 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
               description: "!text-[11px] !-mt-1",
             },
           }}
-        />
-        {/* <GivingDayModal /> */}
+        /> */}
+        <GivingDayModal />
       </div>
     </FeedbackProvider>
   );
